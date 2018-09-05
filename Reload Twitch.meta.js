dbba72f5deff260309b6a0faa543dd12e2544a2a
Maximus Prime <mvdiener@gmail.com>
2018-09-04 17:41:16 -0700

    Updated times for FACEIT Major, London 2018. Adjusted player attributes to check if live.

--- Reload Twitch.meta.js
+++ Reload Twitch.meta.js
@@ -14,13 +14,20 @@
     'use strict';
 
     setInterval(function() {
-        reloadTwitch(15, 19);
-        reloadTwitch(15, 20);
+        reloadTwitch(9, 5);
+        reloadTwitch(9, 6);
+        reloadTwitch(9, 7);
+        reloadTwitch(9, 8);
+        reloadTwitch(9, 9);
+        reloadTwitch(9, 12);
+        reloadTwitch(9, 13);
+        reloadTwitch(9, 14);
+        reloadTwitch(9, 15);
+        reloadTwitch(9, 16);
+        reloadTwitch(14, 20);
         reloadTwitch(15, 21);
         reloadTwitch(15, 22);
-        reloadTwitch(15, 26);
-        reloadTwitch(15, 27);
-        reloadTwitch(19, 28);
+        reloadTwitch(17, 23);
     }, 300000);
 })();
 
@@ -31,14 +38,14 @@ function reloadTwitch(utcHours, utcDayOfMonth) {
     }
 
     //If there is an error on the player, refresh the page
-    if (document.getElementsByClassName('video-player__container')[0].getAttribute('data-screen') == "error") {
-        window.location.reload(true);
-    }
+    //if (document.getElementsByClassName('video-player__container')[0].getAttribute('data-screen') == "error") {
+    //    window.location.reload(true);
+    //}
 
     var now = new Date();
     var isAfterStartTimeOnCorrectDay = (now.getUTCDate() == utcDayOfMonth && now.getUTCHours() >= utcHours);
     
-    if (isAfterStartTimeOnCorrectDay && ((document.getElementsByClassName('video-player__container')[0].getAttribute('data-ended') == "true") || (document.getElementsByClassName('player-streamstatus__label')[0].innerHTML == "Offline"))) {
+    if (isAfterStartTimeOnCorrectDay && (document.getElementsByClassName('player-streamstatus__label').length == 0 || document.getElementsByClassName('player-streamstatus__label')[0].innerHTML != "Live" || document.getElementsByClassName('player-tip--aleft')[0].attributes[1].value == "Play") {
         window.location.reload(true);
     }
 }
